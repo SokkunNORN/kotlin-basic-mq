56a4ca68b2226ee93cfeed579263915c71d7efd6
sokkun <sokkun.norn2019@gmail.com>
2022-11-23 16:55:07 +0700

    feat(core): implement - read message from rabbitmq

--- src/main/kotlin/me/learning/basicmq/rabbitmq/consumer/TransactionConsumer.kt
+++ src/main/kotlin/me/learning/basicmq/rabbitmq/consumer/TransactionConsumer.kt
@@ -1,18 +1,19 @@
 package me.learning.basicmq.rabbitmq.consumer
 
-import me.learning.basicmq.controller.response.TransactionResponse
+import me.learning.basicmq.model.Transaction
 import me.learning.basicmq.rabbitmq.QueueEnum.TRANSFER_QUEUE
 import org.slf4j.LoggerFactory
 import org.springframework.amqp.rabbit.annotation.RabbitHandler
 import org.springframework.amqp.rabbit.annotation.RabbitListener
-import org.springframework.amqp.support.converter.SimpleMessageConverter
+import org.springframework.stereotype.Component
 
+@Component
 @RabbitListener(queues = [TRANSFER_QUEUE])
 class TransactionConsumer {
     val log = LoggerFactory.getLogger(javaClass)
 
     @RabbitHandler
-    fun consumerMessage(response: TransactionResponse) {
-        log.info("The Status Message from queue: ${response::class.java}")
+    fun consumerMessage(response: Transaction) {
+        log.info("Got Message from queue: ${response}")
     }
 }
