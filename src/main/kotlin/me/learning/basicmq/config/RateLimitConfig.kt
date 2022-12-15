d0a6e3e93276d73e5f5c2c866700419ebbf520e5
sokkun <sokkun.norn2019@gmail.com>
2022-12-15 15:33:42 +0700

    feature(core): implement rate limit as annotation

--- src/main/kotlin/me/learning/basicmq/config/RateLimitConfig.kt
+++ src/main/kotlin/me/learning/basicmq/config/RateLimitConfig.kt
@@ -1,4 +1,4 @@
-package me.learning.basicmq.component
+package me.learning.basicmq.config
 
 import io.github.bucket4j.Bandwidth
 import io.github.bucket4j.Bucket
@@ -7,22 +7,23 @@ import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.slf4j.LoggerFactory
-import org.springframework.stereotype.Component
+import org.springframework.context.annotation.Configuration
 import java.time.Duration
 
 @Aspect
-@Component
-class TransactionRateLimitComponent {
+@Configuration
+class RateLimitConfig {
     private val log = LoggerFactory.getLogger(javaClass)
 
     private val bucket = Bucket.builder()
-        .addLimit(Bandwidth.simple(10, Duration.ofSeconds(1)))
+        .addLimit(Bandwidth.simple(10, Duration.ofMinutes(1)))
+        .addLimit(Bandwidth.simple(5, Duration.ofSeconds(20)))
         .build()
 
     @Around("@annotation(me.learning.basicmq.annotation.RateLimit)")
     fun limit(point: ProceedingJoinPoint): Any {
         log.info("checking rate limit...")
-        if (!bucket.tryConsume(10)) throw TooManyRequestsException("")
+        if (!bucket.tryConsume(1)) throw TooManyRequestsException("")
         return point.proceed()
     }
 }
