c0135003b12b5d6b47c09f37c263e5bf909558a8
sokkun <sokkun.norn2019@gmail.com>
2022-12-28 16:08:22 +0700

    feature(core): define rate limit

--- src/main/kotlin/me/learning/basicmq/config/RateLimitConfig.kt
+++ src/main/kotlin/me/learning/basicmq/config/RateLimitConfig.kt
@@ -9,6 +9,7 @@ import org.aspectj.lang.annotation.Aspect
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Configuration
 import java.time.Duration
+import java.time.LocalDateTime
 
 @Aspect
 @Configuration
@@ -16,13 +17,12 @@ class RateLimitConfig {
     private val log = LoggerFactory.getLogger(javaClass)
 
     private val bucket = Bucket.builder()
-        .addLimit(Bandwidth.simple(10, Duration.ofMinutes(1)))
-        .addLimit(Bandwidth.simple(5, Duration.ofSeconds(20)))
+        .addLimit(Bandwidth.simple(10, Duration.ofSeconds(1))) // 10 Request for 1 second
+        .addLimit(Bandwidth.simple(1, Duration.ofMillis(100))) // 100 ms for each request
         .build()
 
     @Around("@annotation(me.learning.basicmq.annotation.RateLimit)")
     fun limit(point: ProceedingJoinPoint): Any {
-        log.info("checking rate limit...")
         if (!bucket.tryConsume(1)) throw TooManyRequestsException("")
         return point.proceed()
     }
