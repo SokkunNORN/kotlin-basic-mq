8bc20622f9944f16444a68ef46a019fc7930c185
sokkun <sokkun.norn2019@gmail.com>
2023-01-13 14:25:55 +0700

    feature(core): define rate limit

--- src/main/kotlin/me/learning/basicmq/config/RateLimitConfig.kt
+++ src/main/kotlin/me/learning/basicmq/config/RateLimitConfig.kt
@@ -9,7 +9,6 @@ import org.aspectj.lang.annotation.Aspect
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Configuration
 import java.time.Duration
-import java.time.LocalDateTime
 
 @Aspect
 @Configuration
